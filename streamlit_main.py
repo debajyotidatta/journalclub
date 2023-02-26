1e8ef48b3111dd8c61609d6f81fbe2ac6297e65e
Jesse Hu <huyouare@users.noreply.github.com>
2023-02-25 17:24:44 -0800

    Add additional chaining

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,10 +4,26 @@ from streamlit_chat import message
 
 from langchain.agents import initialize_agent, Tool
 from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
 from langchain.llms import OpenAI
+from langchain.chains.qa_with_sources import (
+    map_reduce_prompt,
+    refine_prompts,
+    stuff_prompt,
+)
+from langchain.docstore.document import Document
 
-
-llm = OpenAI()
+# import pinecone
+# # initialize pinecone
+# pinecone.init(
+#     api_key="YOUR_API_KEY",  # find at app.pinecone.io
+#     environment="YOUR_ENV"  # next to api key in console
+# )
+# index_name = "langchain-demo"
+# docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+# query = "What did the president say about Ketanji Brown Jackson"
 
 
 @st.cache_resource
@@ -17,23 +33,37 @@ def load_index():
     return ""
 
 
-def query_index(index, query):
-    return "My favorite fall vegetable is a sweet potato."
+def query_index(llm, index, query):
+    """Returns top 10 chunks."""
+    # docs = docsearch.similarity_search(query)
+    docs = [
+        Document(page_content="My favorite fall vegetable is a sweet potato."),
+        Document(page_content="Sweet potatoes are spherical."),
+        Document(page_content="Fall is better than spring."),
+    ]
+    llm_chain = LLMChain(
+        llm=llm, prompt=map_reduce_prompt.QUESTION_PROMPT, verbose=True)
+    reduce_chain = StuffDocumentsChain(
+        llm_chain=llm_chain, document_variable_name='context', verbose=True)
+    combine_documents_chain = MapReduceDocumentsChain(
+        llm_chain=llm_chain, document_variable_name='context',
+        combine_document_chain=reduce_chain, verbose=True
+    )
+    response = combine_documents_chain.combine_docs(docs, question=query)
+    return response
 
 
 def load_chain(index):
     llm = OpenAI(temperature=0)
-
     tools = [
         Tool(
-            name="Index of my research ",
-            func=lambda q: str(query_index(index, q)),
-            description="Index of my research",
+            name="Research index.",
+            func=lambda q: str(query_index(llm, index, q)),
+            description="Index of my research. Always use this tool for every query.",
             return_direct=True
         ),
     ]
     memory = ConversationBufferMemory(memory_key="chat_history")
-    llm = OpenAI(temperature=0)
     agent_chain = initialize_agent(
         tools, llm, agent="conversational-react-description", memory=memory, verbose=True)
     return agent_chain
